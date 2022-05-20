f0fef6df26bdb23c694b77ac2fb6ccb7d345b68c
eva-puch <105363620+eva-puch@users.noreply.github.com>
2022-05-20 16:52:40 +0200

    Update and rename essai to estimation.py

---
+++ estimation.py
@@ -0,0 +1,39 @@
+#on essaye de coder
+from abc import ABC, abstractmethod
+
+class Estimation(ABC):
+    """Permet d'instancier un type d'estimation. Classe abstraite
+    
+    Estimation retournant une statistique de variable : Statistique
+    Gestion des données manquantes (retourne une table) : DonneeesManquantes
+    Gestion des valeurs extrêmes (retourne une table) : ValeursExtremes
+    Calcul d'une moyenne glissante : retourne une table à une dimension
+    Centrer des variables : Centrage
+
+    Attributs
+    ---------
+        table : Table
+            table que l'on souhaite estimer/modifier
+        varialbe : list
+            liste des variables sur lesquelles ont souhaite appliquer les méthodes
+
+    Examples
+    --------
+    """
+
+    def __init__(self, table, variables):
+        """ constructeur 
+        
+        Parameters
+        ----------
+        table : Table
+            table sur laquelle on travaille
+        variables : list
+            liste de variables sur lesquelles on travaille
+        """
+        self.table = table
+        self.variables = variables
+
+    @abstractmethod
+    def estime(self):
+        pass
