f5c4c493b28771ac0e2d31493a4523b60a37e8e8
evavincent14 <80320843+evavincent14@users.noreply.github.com>
2022-05-26 14:36:06 +0200

    Add files via upload

--- jointure.py
+++ jointure.py
@@ -1,22 +1,82 @@
+from table import Table
+from transformation import Transformation
+
 class Jointure(Transformation) :
-    def __init__(self, table2):
+    def __init__(self, table, variables, table2):
+        Transformation.__init__(self, table, variables)
         self.table2 = table2
-        Transformation.__init__()
 
     def transforme(self):
-        #si plus de 1 variable : erreur
-        if len(self.variables):
-            print("Erreur : plus de 1 variable comme clef")
-            return None        
+        #si plus de 1 variable ou pas variable ou nb lignes des tables different : erreur
+        if len(self.variables) > 1 or len(self.variables) == 0 or (len(self.table.donnees) != len(self.table2.donnees)):
+            print("Erreur : il faut 1 variable à filtrer et le même nombre de lignes pour les 2 tables")
+            return Table()     
 
-        #erreur si pas variable clef
-    
-        nouvelle_table = Table([[0]*(len(self.table.donnees[0])+len(self.table2.donnees[0]))]*len(self.table.donnees)))
-        for i in range(len(self.table.donnees[0])):
-            for k in range(len(self.table.donnees)):
-                nouvelle_table.donnees[k][i] = [self.table.donnees[k][i]]
-        for j in range(len(self.table.donnees[0]),len(self.table2.donnees[0])):
-            for l in range(len(self.table.donnees)):
-                nouvelle_table.donnees[l][j] = [self.table2.donnees[l][j-len(self.table.donnees[0])]]
+        nouvelle_table=Table()
+
+        #verifie que variable commun est presente premiere table 
+        comp = 0
+        var = []
+        for n in range(len(self.table.donnees[0])):
+            if self.variables[0] != self.table.donnees[0][n]:
+                comp += 1
+            else :
+                for m in range(len(self.table.donnees)):
+                    var.append(self.table.donnees[m][n])
+        if comp == len(self.table.donnees[0]):
+            print("Erreur : La variable clef n'est pas présente dans la première table")
+            return Table()
+
+        #verifie que variable commun est presente deuxieme table 
+        comp = 0
+        var2 = []
+        for p in range(len(self.table2.donnees[0])):
+            if self.variables[0] != self.table2.donnees[0][p]:
+                comp += 1
+            else :
+                for o in range(len(self.table2.donnees)):
+                    var2.append(self.table2.donnees[o][p])
+        if comp == len(self.table2.donnees[0]):
+            print("Erreur : La variable clef n'est pas présente dans la deuxième table")
+            return Table()
+
+        if var != var2:
+            print("Erreur : Les 2 colonnes de la variable clef ne sont pas les mêmes")
+            return Table()
+
+
+        #on recopie le premier tableau
+        for k in range(len(self.table.donnees)): #parcourt lignes
+            for i in range(len(self.table.donnees[0])): #parcourt colonnes
+                if i == 0:
+                    nouvelle_table.donnees.append([])
+                nouvelle_table.donnees[k].append(self.table.donnees[k][i]) 
+  
+        #on recopie le deuxieme tableau sans la colonne commune
+        for l in range(len(self.table2.donnees)): #parcourt lignes
+            for j in range(len(self.table2.donnees[0])): #parcourt colonnes
+                if self.table2.donnees[0][j] != self.variables[0]:
+                    nouvelle_table.donnees[l].append(self.table2.donnees[l][j]) 
+
         return nouvelle_table
 
+
+
+test = Table([["var1","var2","var3","var4"],
+              ["5","oui","NA","NA"],
+              ["8","non","87","NA"],
+              ["4","oui","2.9","97"]])
+join = Table([["var3","var5","var6","var7"],
+              ["NA","oui","NA","87"],
+              ["87","oui","87","35"],
+              ["2.9","non","65","93"]])
+#nouvelle_table = Table([[0]*(len(test.donnees[0])+len(self.table2.donnees[0]))]*len(self.table.donnees))
+#print(len(test.donnees))
+transf = Transformation(test,["var3"])
+#print(transf.table.donnees)
+#print(join.donnees)
+joindre = Jointure(test, ["var3"], join)
+print(joindre.table.donnees)
+print(joindre.table2.donnees)
+res = joindre.transforme()
+print(res.donnees)
