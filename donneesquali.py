b071449adf7c4fc86249a94bcdb7f8a0ae0bd84a
eva-puch <105363620+eva-puch@users.noreply.github.com>
2022-05-29 08:07:54 +0200

    documentation

--- donneesquali.py
+++ donneesquali.py
@@ -2,6 +2,35 @@ from transformation import Transformation
 from table import Table
 
 class DonneesQuali(Transformation) :
+    """Classe permettant de filtrer les données qualitatives selon les valeurs prises par les variables qualitatives considérées
+    
+    Attributes
+    ---------
+    table : Table
+        liste de listes contenant les données
+    variables : list
+        liste des noms des variables à considérer
+    valeurs : list
+        liste des modalités à conserver de la variable qualitative
+    
+    Example
+    -------
+    >>> t = [["var1","var2","var3","var4"],
+              [5,"oui","NA",76],
+              [8,"non",87,67.9],
+              [4,"oui",2.9,56],
+              [3,"non",66,78.9],
+              [9,"oui",25,"NA"],
+              [8,"non",7.9,13.6]]
+    >>> d = DonneesQuali(t, ['var2'], "oui")
+    >>> print(d.transforme())
+    [["var1","var2","var3","var4"],
+              [5,"oui","NA",76],
+              [4,"oui",2.9,56],
+              [9,"oui",25,"NA"]]
+    
+    
+    """
     def __init__(self, table, variables, valeurs):
         Transformation.__init__(self, table, variables)
         self.valeurs = valeurs
